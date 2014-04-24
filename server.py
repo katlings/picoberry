1c18ea31d9d53c8e8499c3e5b16becb1362755f4
Kathryn Lingel <kathryn.lingel@gmail.com>
2014-04-24 00:01:54 -0700

    Toggle control from a webpage. Too bad the button isn't actually big and red

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 
 import RPi.GPIO as gpio
 
-from flask import Flask, make_response
+from flask import Flask, make_response, render_template
 
 LED_PIN = 24
 gpio.setmode(gpio.BCM)
@@ -11,9 +11,16 @@ gpio.setup(LED_PIN, gpio.OUT)
 app = Flask(__name__)
 app.debug = True
 
+def get_led_state():
+  return gpio.input(LED_PIN)
+
+def state_to_string(state):
+  return "on" if state else "off"
+
 @app.route("/")
-def hello():
-  return "Hello world"
+def control_center():
+  current_state = get_led_state()
+  return render_template("control_center.html", ledState=state_to_string(current_state))
 
 @app.route("/on")
 def turn_on():
@@ -27,10 +34,10 @@ def turn_off():
 
 @app.route("/toggle")
 def toggle():
-  current_state = gpio.input(LED_PIN)
-  gpio.output(LED_PIN, not current_state)
-  new_state = "off" if current_state else "on"
-  return make_response("Light is %s!" % new_state, 200)
+  current_state = get_led_state()
+  new_state = not current_state
+  gpio.output(LED_PIN, new_state)
+  return make_response("Light is %s!" % state_to_string(new_state), 200)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0')
