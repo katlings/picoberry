15b1e866c2b2d17f6e828c1984129e17b6095a9c
Kathryn Lingel <kathryn.lingel@gmail.com>
2014-10-22 22:53:33 -0700

    Adding pulse width modulation; this may or may not actually be correct (is the pin initialized correctly?) but I need to reimage my pi

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import RPi.GPIO as gpio
+from RPIO import PWM
 
 from flask import Flask, make_response, render_template
 
@@ -8,11 +9,21 @@ LED_PIN = 24
 gpio.setmode(gpio.BCM)
 gpio.setup(LED_PIN, gpio.OUT)
 
+servo = PWM.Servo()
+
 app = Flask(__name__)
 app.debug = True
 
+# On/off state is kept as a global variable due to PWM.
+state = gpio.input(LED_PIN)
+
 def get_led_state():
-  return gpio.input(LED_PIN)
+  return state
+
+def set_led_state(new_state):
+  global state
+  state = new_state
+  return state
 
 def state_to_string(state):
   return "on" if state else "off"
@@ -24,11 +35,13 @@ def control_center():
 
 @app.route("/on")
 def turn_on():
+  set_led_state(True)
   gpio.output(LED_PIN, 1)
   return make_response("Light is on!", 200)
 
 @app.route("/off")
 def turn_off():
+  set_led_state(False)
   gpio.output(LED_PIN, 0)
   return make_response("Light is off!", 200)
 
@@ -36,7 +49,12 @@ def turn_off():
 def toggle():
   current_state = get_led_state()
   new_state = not current_state
-  gpio.output(LED_PIN, new_state)
+  set_led_state(new_state)
+  #gpio.output(LED_PIN, new_state)
+  if current_state:
+    servo.stop_servo(LED_PIN)
+  else:
+    servo.set_servo(LED_PIN, 1000)
   return make_response("Light is %s!" % state_to_string(new_state), 200)
 
 if __name__ == "__main__":
